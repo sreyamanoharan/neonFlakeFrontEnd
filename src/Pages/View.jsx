6cbfa37283ff9c982746c509718e5e3f322b4428
aswin <aswin@kssmart.co>
2024-08-02 14:44:51 +0530

    new

--- src/Pages/View.jsx
+++ src/Pages/View.jsx
@@ -20,7 +20,7 @@ const View = () => {
       {tumbnails.map((res, index) => (
         <div key={index} className="relative p-4 flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
           <div className="relative cursor-pointer h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40" onClick={()=>handleNavigate(res?.id)}>
-            <img className="cursor-pointer" src={res?.tumbnail} alt="card-image" />
+            <img className="cursor-pointer" src={res?.thumbnail} alt="card-image" />
           </div>
           <div className="p-6">
             <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
